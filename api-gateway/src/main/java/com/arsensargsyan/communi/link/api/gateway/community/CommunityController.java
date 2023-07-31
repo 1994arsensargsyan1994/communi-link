8078bda7fc047e2827a0eb2cc06e13868fc7c43f
Arsen Sargsyan <arsen.sargsyan@webbfontaine.com>
2023-07-31 21:15:02 +0400

    Changed api documentation props and add new lookup communty details endpont.

--- api-gateway/src/main/java/com/arsensargsyan/communi/link/api/gateway/community/CommunityController.java
+++ api-gateway/src/main/java/com/arsensargsyan/communi/link/api/gateway/community/CommunityController.java
@@ -9,6 +9,7 @@ import com.arsensargsyan.communi.link.api.client.CommunityClient;
 import com.arsensargsyan.communi.link.api.model.request.CommunityCreationRequest;
 import com.arsensargsyan.communi.link.api.model.request.ReservationCreationRequest;
 import com.arsensargsyan.communi.link.api.model.response.CommunityCreationResponse;
+import com.arsensargsyan.communi.link.api.model.response.LookupCommunityDetailsResponse;
 import com.arsensargsyan.communi.link.api.model.response.ReservationCancelResponse;
 import com.arsensargsyan.communi.link.api.model.response.ReservationCreationResponse;
 import com.arsensargsyan.communi.link.common.FailureDto;
@@ -20,6 +21,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -28,6 +30,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -78,6 +81,26 @@ public class CommunityController {
     }
 
     @Operation(
+            summary = "${community.lookup.details.operation.summary}",
+            description = "${community.lookup.details.operation.description}",
+            responses = @ApiResponse(
+                    content = @Content(schema = @Schema(oneOf = LookupCommunityDetailsResponse.class))
+            )
+    )
+    @GetMapping(path = "/community/{id}/details",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public DeferredResult<ResponseEntity<LookupCommunityDetailsResponse>> lookupDetails(
+            @PathVariable("id") @NotNull @Positive Long id
+    ) {
+        logger.info("Lookup community details for given ID: {}.", id);
+        return deferredResponse(
+                CompletableFuture.supplyAsync(() -> communityClient.lookupDetails(id), executor),
+                LookupCommunityDetailsResponse::new
+        );
+    }
+
+    @Operation(
             summary = "${reserve.create.operation.summary}",
             description = "${reserve.create.operation.description}",
             responses = @ApiResponse(
@@ -99,6 +122,13 @@ public class CommunityController {
         );
     }
 
+    @Operation(
+            summary = "${reserve.cancel.operation.summary}",
+            description = "${reserve.cancel.operation.description}",
+            responses = @ApiResponse(
+                    content = @Content(schema = @Schema(oneOf = LookupCommunityDetailsResponse.class))
+            )
+    )
     @DeleteMapping(
             value = "/{communityId}/cancel/{reservationId}"
     )
